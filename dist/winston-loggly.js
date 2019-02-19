51796d5985ad5874b72d3f7662b95d2daf57508b
Onezino Gabriel Moreira <zinogabriel@gmail.com>
2019-02-19 16:34:04 -0300

    build version 3.0.1
    adds new params stripColors and timestamp

--- dist/winston-loggly.js
+++ dist/winston-loggly.js
@@ -1,232 +1,162 @@
-import TransportStream, { TransportStreamOptions } from 'winston-transport';
-import { TransformableInfo } from 'logform';
-import loggly from 'loggly';
-import { format } from 'logform';
-
-interface LogglyTransportOptions extends TransportStreamOptions, loggly.LogglyOptions {
-    
-    //@deprecated
-    id?: any;
-
-    //@deprecated
-    tag?: any;
-
-    /**
-     * uses timestamp meta
-     */
-    timestamp?: boolean;
-
-    stripColors?: boolean;
-}
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const winston_transport_1 = __importDefault(require("winston-transport"));
+const loggly_1 = __importDefault(require("loggly"));
 const CONTEXT_KEYS = ['start', 'from', 'util', 'order', 'callback', 'size', 'format', 'fields'];
-
 const LOGGLY_KEYS = ['query', 'fields', 'start', 'rows', 'limit', 'from', 'util', 'tag'];
-
 const code = /\u001b\[(\d+(;\d+)*)?m/g;
-    
-interface QueryOptions {
-    rows?: number;
-    limit?: number;
-    start?: Date|number;
-    until?: Date|number|string;
-    from?: Date|number|string;
-    order?: string;
-
-    [key: string]: any;
-    
-}
-
-export class Loggly extends TransportStream {
-
-    name: string;
-
-    client: loggly.LogglyInstance;
-
-    tags: string[];
-
-    timestamp: boolean = false;
-
-    stripColors: boolean = false; 
-
-    constructor(options: LogglyTransportOptions = {
+class Loggly extends winston_transport_1.default {
+    constructor(options = {
         token: '',
         subdomain: '',
     }) {
         super(options);
-        this.name   = 'loggly';
-        
+        this.timestamp = false;
+        this.stripColors = false;
+        this.name = 'loggly';
         if (!options.token || !options.subdomain) {
             throw new Error('Loggly subdomain/token is required.');
-        };
-        
+        }
+        ;
         //deprecated tag / id attributes
         if (options.tag || options.id) {
             throw new Error('Please use tags: string[] isntead of id or tag');
         }
         this.tags = options.tags || [];
-
         if (options.timestamp) {
             this.timestamp = options.timestamp;
         }
-
         if (options.stripColors) {
             this.stripColors = options.stripColors;
         }
-
-        this.client = loggly.createClient(options);
+        this.client = loggly_1.default.createClient(options);
     }
-
-    log(info: TransformableInfo | string | null, callback?: () => void) {
+    log(info, callback) {
         if (this.silent) {
-            if (callback) setImmediate(callback);
+            if (callback)
+                setImmediate(callback);
             return;
         }
-        
-
-        const logged = (err: any, res: any) => {
+        const logged = (err, res) => {
             if (err) {
                 this.emit('warn', err);
-            } else {
+            }
+            else {
                 this.emit('logged', info);
             }
-        }
-
-        let meta: TransformableInfo = {
+        };
+        let meta = {
             level: 'INFO',
             message: '',
             tags: [],
-        }
-
+        };
         if (typeof info === 'string') {
             meta.message = info;
-        } else if (info && 'level' in info) {
+        }
+        else if (info && 'level' in info) {
             meta = info;
         }
-
         if (this.timestamp && !meta.timestamp) {
             meta.timestamp = (new Date()).toISOString();
         }
-
         if (this.stripColors) {
             meta.message = ('' + meta.message).replace(code, '');
         }
         this.client.log(meta, this.tags, logged);
-        
         if (callback) {
             setImmediate(callback);
         }
     }
-
-    _extractContext( options: QueryOptions = {}) {
-
-        let context: QueryOptions = {};
-
+    _extractContext(options = {}) {
+        let context = {};
         //extrai atributos do options
-        CONTEXT_KEYS.forEach( key => {
+        CONTEXT_KEYS.forEach(key => {
             context[key] = options[key];
             delete options[key];
         });
-
         context = this.normalizeQuery(context);
         if (context.from && context.from instanceof Date) {
             context.from = context.from.toISOString();
         }
-        
         if (context.until && context.until instanceof Date) {
             context.until = context.until.toISOString();
         }
-        
-
-        context.from  = context.from  || '-1d';
+        context.from = context.from || '-1d';
         context.until = context.until || 'now';
-        context.size  = context.size  || 50;
-
+        context.size = context.size || 50;
         return context;
     }
-
-    query(options: QueryOptions, callback: (err: any, result: any) => void) {
+    query(options, callback) {
         const context = this._extractContext(options);
-        
         let logglyOptions = this._logligy(options);
         logglyOptions = Object.assign({}, logglyOptions, context);
-
         this.client
             .search(logglyOptions, (err, result) => {
-                if (err) {
-                    callback(err, []);
-                } else {
-                    callback(null, result);
-                }
-            })
+            if (err) {
+                callback(err, []);
+            }
+            else {
+                callback(null, result);
+            }
+        })
             .run((err, logs) => {
-                return err? callback(err, null) : callback(null, this.sanitizeLogs(logs));
-            });
+            return err ? callback(err, null) : callback(null, this.sanitizeLogs(logs));
+        });
     }
-
-    sanitizeLogs<T>(logs: T): T {
+    sanitizeLogs(logs) {
         return logs;
     }
-
-    normalizeQuery(options: QueryOptions) {
+    normalizeQuery(options) {
         const defaultQueryOptions = {
             rows: 10,
             limit: 10,
             start: 0,
             util: new Date(),
             order: 'desc',
-        }
-
+        };
         const normalizedOptions = Object.assign({}, defaultQueryOptions, options);
         // limit
         normalizedOptions.rows = options.rows || options.limit || 10;
-  
         // starting row offset
         normalizedOptions.start = normalizedOptions.start || 0;
-  
         // now
         normalizedOptions.until = normalizedOptions.until || new Date();
         if (typeof normalizedOptions.until !== 'object') {
-          normalizedOptions.until = new Date(normalizedOptions.until);
+            normalizedOptions.until = new Date(normalizedOptions.until);
         }
-  
-        const until: unknown = normalizedOptions.until;
+        const until = normalizedOptions.until;
         // now - 24
-        normalizedOptions.from = normalizedOptions.from || ((until as number) - (24 * 60 * 60 * 1000));
+        normalizedOptions.from = normalizedOptions.from || (until - (24 * 60 * 60 * 1000));
         if (typeof normalizedOptions.from !== 'object') {
-          normalizedOptions.from = new Date(normalizedOptions.from);
+            normalizedOptions.from = new Date(normalizedOptions.from);
         }
-  
         // 'asc' or 'desc'
         normalizedOptions.order = normalizedOptions.order || 'desc';
-  
         return normalizedOptions;
     }
-
-    _logligy(options: QueryOptions): loggly.SearchOptionsWithQuery {
-        const joinner: string[] = [];
-
+    _logligy(options) {
+        const joinner = [];
         const keys = Object.keys(options);
-        keys.forEach( key => {
+        keys.forEach(key => {
             //ignora na lista de ignores
             if (LOGGLY_KEYS.indexOf(key) > -1) {
                 return;
             }
-            
             const val = options[key];
-            
             joinner.push(`json.${key}:${val}`);
         });
-
         //adiciona tag se existir
         if (options.tag) {
             joinner.push(`tag:${options.tag}`);
         }
-        
         if (options.query) {
             joinner.unshift(options.query);
         }
-
         return { query: joinner.join(' AND ') };
     }
 }
+exports.Loggly = Loggly;
+//# sourceMappingURL=winston-loggly.js.map
